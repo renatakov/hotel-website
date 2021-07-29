143c37efd941dc2f6b0034bef1ada2f5aa0a68ed
renatakov <renatakov1010@gmail.com>
2021-07-29 19:32:42 +0300

    gg

--- src/pages/TheHut/TheHut.js
+++ src/pages/TheHut/TheHut.js
@@ -5,9 +5,9 @@ class TheHut extends Component{
   render() {
     return(
       <>
-      <div classsName={csss.thehutdiv}>
-        <img className={csss.imgthehut} src="https://i.pinimg.com/originals/b8/ff/59/b8ff59f93b8b250b6ac07f99938a0200.jpg" alt="" />
-        <div className={csss.overlay12}></div>
+      <div classsName={csss.thehut1}>
+        {/* <img className={csss.imgthehut} src="https://i.pinimg.com/originals/b8/ff/59/b8ff59f93b8b250b6ac07f99938a0200.jpg" alt="" />
+        <div className={csss.overlay12}></div> */}
         <h1 className={csss.thehuth1}>The Hut</h1>
       </div>
       </>
