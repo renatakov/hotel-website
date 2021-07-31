fe842e7f1ce60bca66ef87c6521acf91f897bced
renatakov <renatakov1010@gmail.com>
2021-07-31 22:54:45 +0300

    the area 18%

--- src/pages/TheArea/TheArea.js
+++ src/pages/TheArea/TheArea.js
@@ -10,6 +10,12 @@ class TheArea extends Component {
         <p className={cssstyle.p1area}>Vesterborg, Denmark</p>
         <button className={cssstyle.buttonsmall}>GET DIRECTIONS</button>
       </section>
+      <section className={cssstyle.sec2area}>
+        <img className={cssstyle.firstimgarea} src="https://live.staticflickr.com/65535/48839891426_68f5affaa0_b.jpg" alt="Forest Outside" />
+        <h2 className={cssstyle.h2area}>Vesterborg</h2>
+        <q className={cssstyle.q1area}>Lorem ipsum dolor sit amet, consectetur adipiscing...</q>
+        <p className={cssstyle.p2area}>Løvtag’s three exclusive and comfortable cabins, Et, Ro and Ly, are designed by architect Sigurd Larsen.<br /> They all have an open space with a double bed, a double sofa bed, kitchen, separate toilet and an outdoor shower.<br /> On the roof, surrounded by treetops, there is a terrace, which is about nine meters above ground.</p>
+      </section>
       </>
     );
   }
