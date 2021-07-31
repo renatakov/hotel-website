5cb0979e5ee0393f93a6ef91227fb049cef2ab34
renatakov <renatakov1010@gmail.com>
2021-07-31 20:55:35 +0300

    1090553535%

--- src/pages/TheHut/TheHut.js
+++ src/pages/TheHut/TheHut.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import csss from "./TheHut.module.css";
 
-class TheHut extends Component{
+class TheHut extends Component {
   render() {
-    return(
+    return (
       <>
-      <div className={csss.thehut1}>
-        <div className={csss.overlay12}></div>
-        <h1 className={csss.thehuth1}>The Hut</h1>
-      </div>
+        <div className={csss.thehut1}>
+          <div className={csss.overlay12}></div>
+          <h1 className={csss.thehuth1}>The Hut</h1>
+        </div>
       </>
     );
   }
