5374f7e7965d9d5e50daa8cedf59f81f101ece39
renatakov <renatakov1010@gmail.com>
2021-08-06 10:52:17 +0300

    the hut 80%%%

--- src/pages/TheHut/TheHut.js
+++ src/pages/TheHut/TheHut.js
@@ -26,6 +26,25 @@ class TheHut extends Component {
             <br /> On the roof, surrounded by treetops, there is a terrace,
             which is about nine meters above ground.
           </p>
+          <p className={csss.p5thehut}>The Inside</p>
+          <p className={csss.p6thehut}>Space</p>
+          <p className={csss.p7thehut}>Kitchen</p>
+          <p className={csss.p8thehut}>Bathroom</p>
+          <p className={csss.p9thehut}>
+            Løvtag’s three exclusive and comfortable cabins, Et, Ro and Ly, are
+            designed by architect Sigurd Larsen.
+            <br /> They all have an open space with a double bed, a double sofa
+            bed, kitchen, separate toilet and an outdoor shower.
+            <br /> On the roof, surrounded by treetops, there is a terrace,
+            which is about nine meters above ground.
+            <br /> The cottages are built around tall, old trees that go through
+            the entire cottage from floor to ceiling.
+          </p>
+          <img
+            src="https://images.dwell.com/photos-6575684823601102848/6651420397127380992-large/watch-the-northern-lights-from-the-comfort-of-your-warm-bed-at-panorama-glass-lodge-iceland-designed-by-the-estonian-company-ood-homes-the-two-200-square-foot-prefab-cabins-are-thoughtfully-made-for-small-space-living-each-has-a-bedroom-living-room-bathro.jpg"
+            alt="Second Img The Hut"
+            className={csss.secondimgthehut}
+          />
         </section>
       </>
     );
