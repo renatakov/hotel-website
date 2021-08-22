453edd6f8cada1c1b995589a4b6876ce290dac40
renatakov <renatakov1010@gmail.com>
2021-08-22 18:23:30 +0300

    booking is 90%

--- src/pages/Booking/Booking.js
+++ src/pages/Booking/Booking.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 import css from "./Booking.module.css";
+import paypal from "../images/paypal.svg";
+import mastercard from "../images/mastercard.svg";
+import visa from "../images/visa.svg";
 
 class Booking extends Component {
   render() {
@@ -12,8 +16,74 @@ class Booking extends Component {
         <section className={css.sec2booking}>
           <p className={css.p2booking}>Choose Additional Services</p>
           {/* TODO: сделать букинг(booking) */}
-
+          <h4 className={css.h4booking}>Classic Dinner</h4>
+          <p className={css.p3booking}>
+            Bread, Coffee, milk, juice, and a selection of spreads + toppings.
+          </p>
         </section>
+        <section className={css.sec3booking}>
+          <p className={css.p4booking}>Hut rules</p>
+          <p className={css.p5booking}>
+            Check-in: 2:00 PM - 11:00 PM <br /> Checkout: 11:00 AM <br /> Not
+            suitable for children and infants <br /> No smoking <br /> No pets
+          </p>
+        </section>
+        <section className={css.sec3booking}>
+          <p className={css.p6booking}>Cancellation policy</p>
+          <p className={css.p7booking}>
+            Free cancellation until 1:00 PM on Mar 27
+            <br /> After that, cancel before 2:00 PM on Apr 1 and get a 50%
+            refund, minus the first night
+            <br /> and service fee.
+          </p>
+        </section>
+        <section className={css.sec4booking}>
+          <p className={css.p8booking}>Important Information</p>
+          <p className={css.p9booking}>
+            You need to hike a steep hill to arrive at the treehouses, it takes
+            approx 20-30 minutes
+            <br /> on a trail with stairs and uneven ground.
+          </p>
+        </section>
+
+        <NavLink to="/faq">
+          <button className={css.faqbooking}>READ FAQ</button>
+        </NavLink>
+        <footer>
+          <p className={css.footerspb}>TERMS AND CONDITIONS</p>
+          <p className={css.footerspb2}>CENCELLATION POLICY</p>
+          <p className={css.footerspb3}>PRIVACY POLICY</p>
+          <img
+            src={paypal}
+            className={css.paypalbook}
+            alt="Paypal"
+            width="50px"
+            height="50px"
+          />
+          <img
+            src={mastercard}
+            className={css.mastercardbook}
+            alt="Mastercard"
+            width="50px"
+            height="50px"
+          />
+          <img
+            src={visa}
+            className={css.visabook}
+            alt="visa"
+            width="50px"
+            height="50px"
+          />
+          <img
+            src="https://shop.sembenini.it/img/cms/bank-transfer-logo-on-black.png"
+            className={css.banktransferbook}
+            alt="v"
+            width="50px"
+            height="50px"
+          />
+          <p className={css.contentbook}>© 2021 The Glass Huts</p>
+        </footer>
+        <section className={css.sec5booking}></section>
       </>
     );
   }
