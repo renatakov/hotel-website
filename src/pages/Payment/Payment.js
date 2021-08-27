e620c27c306060a0e815c0fdb211a3f13bdae0bd
renatakov <renatakov1010@gmail.com>
2021-08-27 13:56:34 +0300

    payment is 80%

--- src/pages/Payment/Payment.js
+++ src/pages/Payment/Payment.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import stylecss from "./Payment.module.css";
 import { NavLink } from "react-router-dom";
+import paypal from "../images/paypal.svg";
+import mastercard from "../images/mastercard.svg";
+import visa from "../images/visa.svg";
+import card from "../images/credit-card-solid.svg";
 
 class Payment extends Component {
   render() {
@@ -8,20 +12,79 @@ class Payment extends Component {
       <>
         <section className={stylecss.s1payment}>
           <NavLink to="/booking">
-          <p className={stylecss.p1payment}>&#8592;CONFIRM & PAY</p>
+            <p className={stylecss.p1payment}>&#8592;CONFIRM & PAY</p>
           </NavLink>
         </section>
         <section className={stylecss.s2payment}>
           <p className={stylecss.p2payment}>Personal Information</p>
-          <input id={stylecss.firstnamepay} type="text" placeholder="First Name" />
-          <input id={stylecss.lastnamepay} type="text" placeholder="Last Name" />
+          <input
+            id={stylecss.firstnamepay}
+            type="text"
+            placeholder="First Name"
+          />
+          <input
+            id={stylecss.lastnamepay}
+            type="text"
+            placeholder="Last Name"
+          />
           <input id={stylecss.emailpay} type="email" placeholder="Email" />
           <input id={stylecss.phonepay} type="tel" placeholder="Phone Number" />
           <input id={stylecss.addresspay} type="text" placeholder="Address" />
-          <input id={stylecss.postalcodepay} type="text" placeholder="Postal Code" />
+          <input
+            id={stylecss.postalcodepay}
+            type="text"
+            placeholder="Postal Code"
+          />
           <input id={stylecss.citypay} type="text" placeholder="City" />
           <input id={stylecss.countrypay} type="text" placeholder="Country" />
         </section>
+        <section className={stylecss.s3payment}>
+          <p className={stylecss.p3payment}>Pay with</p>
+          <input
+            id={stylecss.creditcardpay}
+            type="text"
+            placeholder="Credit or Debit card"
+          />
+          <input id={stylecss.cardnumber} type="text" placeholder="Card Number" />
+          <input id={stylecss.expiration} type="text" placeholder="Expiration" />
+          <input id={stylecss.ccv} type="text" placeholder="CCV" />
+          <input id={stylecss.zipcode} type="text" placeholder="Zip Code" />
+          <input id={stylecss.countrypay1} type="text" placeholder="Country" />
+        </section>
+        <footer>
+          <p className={stylecss.footersp}>TERMS AND CONDITIONS</p>
+          <p className={stylecss.footersp2}>CENCELLATION POLICY</p>
+          <p className={stylecss.footersp3}>PRIVACY POLICY</p>
+          <img
+            src={paypal}
+            className={stylecss.paypal}
+            alt="Paypal"
+            width="50px"
+            height="50px"
+          />
+          <img
+            src={mastercard}
+            className={stylecss.mastercard}
+            alt="Mastercard"
+            width="50px"
+            height="50px"
+          />
+          <img
+            src={visa}
+            className={stylecss.visa}
+            alt="visa"
+            width="50px"
+            height="50px"
+          />
+          <img
+            src="https://shop.sembenini.it/img/cms/bank-transfer-logo-on-black.png"
+            className={stylecss.banktransfer}
+            alt="v"
+            width="50px"
+            height="50px"
+          />
+          <p className={stylecss.content}>© 2021 The Glass Huts</p>
+        </footer>
       </>
     );
   }
